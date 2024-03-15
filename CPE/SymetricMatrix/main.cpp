e21d95da1ffc0bcc607cfa2619af87d3b4f69d2d
Sifiso Lucolo Dhlamini <sifisoldhlamini@outlook.com>
2024-03-15 17:46:43 +0800

    Clean-up

--- CPE/SymetricMatrix/main.cpp
+++ CPE/SymetricMatrix/main.cpp
@@ -1,8 +1,3 @@
-// check matrix symetricy
-// METHOD:
-// 1.Change rows to columns in copy matrix
-// 2. Check if all elements are the same while traversing through the both matrices
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -11,7 +6,6 @@ using namespace std;
 
 int main()
 {
-
     int cases, size;
 
     cin >> cases;
@@ -22,8 +16,6 @@ int main()
 
         vector<vector<int>> originalMatrix(size, vector<int>(size));
         vector<vector<int>> compareMatrix(size, vector<int>(size));
-        // int originalArr[size][size];
-        // int compareArr[size][size];
 
         // populate original array
         for (int i = 0; i < size; i++)
@@ -53,33 +45,34 @@ int main()
                 }
             }
         }
-
-        // save transposed version
-        // for (int i = 0; i < size; i++)
-        // {
-        //     for (int j = 0; j < size; j++)
-        //     {
-        //         // need to change this
-        //         compareMatrix[i][j] = originalMatrix[size - i - 1][size - j - 1];
-        //     }
-        // }
-
-        // print compare matrix
-        // for (int i = 0; i < size; i++)
-        // {
-        //     for (int j = 0; j < size; j++)
-        //     {
-        //         cout << compareMatrix[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        // compare vector equality
-        // bool is_equal = equal(originalMatrix.begin(), originalMatrix.end(), compareMatrix.begin());
-
-        // (is_equal) ? cout << "Symetric" << endl : cout << "Non-symetric" << endl;
-
         (isSymetric) ? cout << "Symetric" << endl : cout << "Non-symetric" << endl;
     }
-
     return 0;
 }
+// check matrix symetricy
+// METHOD:
+// 1.Change rows to columns in copy matrix
+// 2. Check if all elements are the same while traversing through the both matrices
+// save transposed version
+// for (int i = 0; i < size; i++)
+// {
+//     for (int j = 0; j < size; j++)
+//     {
+//         // need to change this
+//         compareMatrix[i][j] = originalMatrix[size - i - 1][size - j - 1];
+//     }
+// }
+
+// print compare matrix
+// for (int i = 0; i < size; i++)
+// {
+//     for (int j = 0; j < size; j++)
+//     {
+//         cout << compareMatrix[i][j] << " ";
+//     }
+//     cout << endl;
+// }
+// compare vector equality
+// bool is_equal = equal(originalMatrix.begin(), originalMatrix.end(), compareMatrix.begin());
+
+// (is_equal) ? cout << "Symetric" << endl : cout << "Non-symetric" << endl;
